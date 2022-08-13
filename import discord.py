5e72b4112654ae28975de49f40dc3e5fe8b44fb8
David Gordon <davb.gordon@gmail.com>
2022-08-13 15:16:28 +0100

    v1

---
+++ import discord.py
@@ -0,0 +1,29 @@
+import discord
+import re
+
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if "CEST" in message.content:
+        try:
+            time = re.findall("\d+", message.content)[0]
+        except:
+            return
+        print(time)
+        time = int(time)
+        if time > 23:
+            return
+        time_message = "British Standard Time " + str(time -1) + "00\n" + "Eastern Standard Time " + str(time - 6) + "00\n" + "Arabian Standard Time " + str(time + 1) +"00"
+
+        await message.channel.send(time_message)
+
+client.run("MTAwNzY5NjI3OTkwOTk2OTk3MA.G3ZfgY.192Ei6MvhQBmHCHKdLg070zC-hM-Og5MdSHs8o")
