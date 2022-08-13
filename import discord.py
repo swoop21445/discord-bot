6d0e94fae0f6726d945d47befb8d56aeff44a946
David Gordon <davb.gordon@gmail.com>
2022-08-13 18:02:05 +0100

    all timezones accepted as input

--- import discord.py
+++ import discord.py
@@ -1,8 +1,10 @@
 import discord
 import re
-
+import secrets
 
 client = discord.Client()
+
+time_relationships = {"GMT":0, "CEST" : 2, "BST" : 1, "EST": -5, "AST":3}
 
 @client.event
 async def on_ready():
@@ -12,18 +14,25 @@ async def on_ready():
 async def on_message(message):
     if message.author == client.user:
         return
-
-    if "CEST" in message.content:
+    if "CEST" or "BST" or "EST" or "AST" in message.content:
         try:
             time = re.findall("\d+", message.content)[0]
         except:
             return
-        print(time)
         time = int(time)
-        if time > 23:
+        if time > 23 and time < 1000:
             return
-        time_message = "British Summer Time " + str(time -1) + "00\n" + "Eastern Standard Time " + str(time - 6) + "00\n" + "Arabian Standard Time " + str(time + 1) +"00"
+        zone = re.findall("[A-Z]+", message.content)[0]
+        try:
+            gmt = time - time_relationships[zone]
+        except:
+            return
+        cest = gmt + time_relationships["CEST"]
+        bst = gmt + time_relationships["BST"]
+        est = gmt + time_relationships["EST"]
+        ast = gmt + time_relationships["AST"]
+        time_message = "Central European Summer Time " + str(cest) + "00\n"+"British Summer Time " + str(bst) + "00\n" + "Eastern Standard Time " + str(est) + "00\n" + "Arabian Standard Time " + str(ast) +"00\n"+ "Greenwich Mean Time " + str(gmt) + "00\n"
 
         await message.channel.send(time_message)
 
-client.run("")
+client.run(secrets.token)
